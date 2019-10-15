0188dd5d434da5aa5de9d299438b4332f83ca684
wolfishness <961053359@qq.com>
2019-10-15 15:13:08 +0800

    rabbit测试消费者

---
+++ rabbitmq_consumer/src/main/java/com/ljs/consumer/consumer/HelloReceiver2.java
@@ -0,0 +1,23 @@
+package com.ljs.consumer.consumer;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName: HelloReceiver
+ * @Description: TODO
+ * @Author: lujinshan
+ * @Date: 2019/10/15 14:09
+ * @Version: 1.0.0
+ */
+@Component
+@RabbitListener(queues = "hello")
+public class HelloReceiver2 {
+
+    @RabbitHandler
+    public void process(String hello) {
+        System.out.println("Receiver2  : " + hello);
+    }
+
+}
